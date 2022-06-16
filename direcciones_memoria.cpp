91ff92ac63c2bd784dbdb83ee7903c9c19c7f73f
Armando Codigos <rcanizales@uca.edu.sv>
2022-06-16 09:23:02 -0600

    Añadir ejercicio 16junio

---
+++ direcciones_memoria.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int variable = 3;
+
+    cout << "Valor contenido dentro de la variable: " << variable << endl;
+    cout << "Direccion de memoria de la variable: " << &variable << endl;
+
+    return 0;
+}
